7bd50c64d1d8c6fbad3853f50737a2a978975503
jocastro <jocastro@bancolombia.com.co>
2023-05-26 14:44:04 -0500

    funcionando 100%

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule }from '@angular/core'; 
+import {BrowserModule }from '@angular/platform-browser'; 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import {AppRoutingModule }from './app-routing.module'; 
+import {AppComponent }from './app.component'; 
+import {UserComponent }from './user/user.component'; 
+import {HttpClientModule }from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms';
 
-@NgModule({
-  declarations: [
-    AppComponent,
+@NgModule( {
+declarations:[
+    AppComponent, 
     UserComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ], 
+imports:[
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule, 
+    FormsModule,
+], 
+providers:[], 
+bootstrap:[AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit }from '@angular/core'; 
+import {UserService }from '../services/user.service'; 
+import {NgForm }from '@angular/forms'; 
+import {User }from '../models/user'; 
 
-@Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+declare var M:any; 
+@Component( {
+selector:'app-user', 
+templateUrl:'./user.component.html', 
+styleUrls:['./user.component.css'], 
+ providers: [UserService]
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+constructor(public userService:UserService) {}
+
+ngOnInit():void {
+}
+
+addUser(form?:NgForm) {
+//console.log(form?.value);
+this.userService.PostUser(form?.value)
+//.subscribe(res => {console.log(res)});
+.subscribe(res =>  {
+this.resetForm(form); 
+M.toast( {html:'Usuario agregado correctamente'}); 
+}); 
+}
+
+resetForm(form?:NgForm) {// Limpiar el formulario, recibe un formulario como parametro
+if (form) {
+form.reset(); 
+this.userService.selectedUser = new User(); 
+}
+}
 
 }

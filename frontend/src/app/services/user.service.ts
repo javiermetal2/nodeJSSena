554bd4f8a00a24c38ed6cbeaf18362ab72cef98d
jocastro <jocastro@bancolombia.com.co>
2023-05-28 08:18:18 -0500

    listo

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -24,13 +24,13 @@ return this.http.get < User[] > (this.URL_API);
 PostUser(euser:User) {
 if (euser._id === "") {return this.http.post(this.URL_API, euser); 
 }else {
-return this.http.put(this.URL_API + `/$ {euser._id}`, euser);
+return this.http.put(this.URL_API + "/"+euser._id, euser);
 }
 
 }
 
 putUser(eUser:User) {
-return this.http.put(this.URL_API + `/$ {User._id}`, User); // se utiliza la version de ecmascript, debemos darle el id del User
+return this.http.put(this.URL_API  + "/"+ eUser._id, eUser); // se utiliza la version de ecmascript, debemos darle el id del User
 }
 
 deleteUser(_id:string) {// Solo necesito el id, no todo lo del User

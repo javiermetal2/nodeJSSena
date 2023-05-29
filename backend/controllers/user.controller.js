39a320ddf53c7dc65b24bbb8bbc02893633045a2
jocastro <jocastro@bancolombia.com.co>
2023-05-29 10:43:20 -0500

    corregido el editar

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,42 +1,41 @@
-const User = require('../models/User');
+const User = require('../models/User'); 
 
-const userCtrl = {};
+const userCtrl =  {}; 
 
-userCtrl.getUsers = async (req, res) => {
+userCtrl.getUsers = async (req, res) =>  {
    
-      const users = await User.find();
-      res.json(users);
+      const users = await User.find(); 
+      res.json(users); 
    }
 
-userCtrl.createUser = async (req, res) => {
-      const user = new User(req.body);
-      await user.save();
+userCtrl.createUser = async (req, res) =>  {
+      const user = new User(req.body); 
+      await user.save(); 
 
-      res.json({ 'status': 'User saved' });
+      res.json( {'status':'User saved'}); 
    }
 
-userCtrl.getUserById = async (req, res) => {
-      const user = await User.findById(req.params.id);
+userCtrl.getUserById = async (req, res) =>  {
+      const user = await User.findById(req.params.id); 
 
-      res.json(user);
+      res.json(user); 
    }
 
-userCtrl.userEdit = async (req, res) => {
-      const { id } = req.param;
-      const user = {
-         name: req.body.name,
-         username: req.body.username,
-         password: req.body.password,
-         updatedAt: req.body.updatedAt
-      };
-      await User.findByIdAndUpdate(id, { $set: user }, { new: true });
-      res.json({ status: 'User Updated' });
+userCtrl.userEdit = async (req, res) =>  {
+      const user =  {
+         name:req.body.name, 
+         username:req.body.username, 
+         password:req.body.password, 
+         updatedAt:req.body.updatedAt
+      }; 
+   await User.findByIdAndUpdate(req.params.id,  {$set:user },  {new:true }); 
+      res.json( {status:'User Updated'}); 
    }
 
-userCtrl.deleteUser = async (req, res) => {
-      await User.findByIdAndDelete(req.params.id);
-      res.json({ status: 'User Deleted' });
+userCtrl.deleteUser = async (req, res) =>  {
+      await User.findByIdAndDelete(req.params.id); 
+      res.json( {status:'User Deleted'}); 
    }
 
    //exporto el módulo
-module.exports = userCtrl;
+module.exports = userCtrl; 
